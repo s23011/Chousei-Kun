2b9690b4a29caf4686f9e19990e1780bfd936832
s23011 <s23011@st.kic.ac.jp>
2024-05-09 16:28:19 +0900

    change_db_connection_to_ignore

--- model/connection.php
+++ model/connection.php
@@ -1,7 +1,7 @@
 <?php
 $dsn = 'mysql:dbname=chouseikun;host=localhost;charset=utf8;';
 try{
-    $GLOBALS["pdo"] = new PDO($dsn,'root','kickickic');
+    $GLOBALS["pdo"] = new PDO($dsn,'root','6251');
 }catch(PDOException $e){
     return set_db_msg('Failed to connect to MySQL:'.$e->getMessage());
 }
